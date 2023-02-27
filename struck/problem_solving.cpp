642e0b949ac2c28fea779f00e82d464110829478
vous00 <100505460+vous00@users.noreply.github.com>
2023-02-27 12:54:10 +0900

    문제풀이

---
+++ struck/problem_solving.cpp
@@ -0,0 +1,560 @@
+// 백준 10828
+/*#include <iostream>
+#include <string>
+#define SIZE 10000
+
+using namespace std;
+
+class Stack
+{
+
+private:
+    int *arr;
+    int top;
+    int capacity; // 스택의 용량
+
+public:
+    Stack(int size = SIZE);
+    ~Stack();
+
+    void push(int x);
+    int pop();
+    int size();
+    bool isEmpty();
+    bool isFull();
+    int peek();
+};
+
+Stack::Stack(int size)
+{ // 생성자 초기화
+
+    arr = new int[size];
+    capacity = size;
+    top = -1;
+}
+
+Stack::~Stack()
+{ // 소멸자 초기화
+    delete[] arr;
+}
+
+void Stack::push(int x)
+{ // 삽입
+
+    if (isFull())
+    {
+        cout << "스택이 가득 차있습니다.\n";
+    }
+    else
+    {
+        arr[++top] = x;
+    }
+}
+
+int Stack::pop()
+{ // 삭제
+
+    if (isEmpty())
+    {
+       top=0;
+       cout<<-1<<endl;
+    }
+    else
+    {
+        cout<<peek()<<endl;
+    }
+    return arr[top--];
+}
+
+int Stack::size()
+{ // 스택의 용량
+
+    return top +1;
+}
+
+bool Stack::isEmpty()
+{ // 비어있을 때
+
+    return top == -1;
+}
+
+bool Stack::isFull()
+{ // 가득 차있을 때
+    return top == capacity - 1;
+}
+
+int Stack::peek()
+{
+
+    if (!isEmpty())
+    {
+        return arr[top];
+    }
+    else
+    {   int a = -1;
+        return a;
+    }
+}
+
+int main()
+{
+
+    int num;
+    cin >> num;
+
+    Stack st;
+
+    for (int i = 0; i < num; i++)
+    {
+
+        string l;
+        cin >> l;
+
+        if (l == "push")
+        {
+            int p;
+            cin >> p;
+            st.push(p);
+        }
+        else if (l == "pop")
+        {
+            st.pop();
+        }
+        else if (l == "size")
+        {
+            cout<<st.size()<<endl;
+        }
+        else if (l == "empty")
+        {
+            cout << st.isEmpty() << endl;
+
+        }
+        else if (l == "top")
+        {
+            cout << st.peek() << endl;
+        }
+    }
+
+    return 0;
+}*/
+
+// 백준 10845
+/*#include <iostream>
+#include <string>
+#define SIZE 10000
+
+using namespace std;
+
+class Q
+{
+private:
+    int *arr;
+    int fron;
+    int rear;
+    int count;
+    int capacity;
+public:
+    Q(int size = SIZE);
+    ~Q();
+
+    void push(int x);
+    int pop();
+    int size();
+    bool empty();
+    bool full();
+    int front();
+    int back();
+};
+
+Q::Q(int size)
+{
+    capacity=size;
+    arr = new int[size];
+    fron = -1;
+    rear = -1;
+    count = 0;
+}
+Q::~Q()
+{
+    delete[] arr;
+}
+
+bool Q::empty()
+{
+    return (rear == fron);
+}
+
+int Q ::size()
+{
+
+    return count;
+}
+bool Q::full()
+{
+    return (size()==capacity);
+}
+
+void Q::push(int x)
+{
+    if (full())
+    {
+        cout << "큐가 가득 차있습니다.\n";
+    }
+    else
+    {
+        arr[++rear] = x;
+        count++;
+    }
+}
+
+int Q::pop()
+{
+
+    if (empty())
+    {
+        cout << "-1\n";
+    }
+    else
+    {
+        cout << arr[++fron] << endl;
+        count--;
+    }
+
+    return 0;
+    ;
+}
+
+int Q::front()
+{
+    if (empty())
+    {
+        cout << "-1" << endl;
+        return 0;
+    }
+    else
+        cout << arr[fron + 1] << endl;
+    return 0;
+}
+
+int Q::back()
+{
+    if (empty())
+    {
+        int a = -1;
+        return -1;
+    }
+    else
+        return arr[rear];
+}
+
+int main()
+{
+
+    int m;
+    cin >> m;
+
+    Q q;
+
+    for (int i = 0; i < m; i++)
+    {
+        string a;
+        cin >> a;
+
+        if (a == "push")
+        {
+            int num;
+            cin >> num;
+            q.push(num);
+        }
+        else if (a == "pop")
+        {
+            q.pop();
+        }
+        else if (a == "size")
+        {
+            cout << q.size() << endl;
+        }
+        else if (a == "empty")
+        {
+            cout << q.empty() << endl;
+        }
+        else if (a == "front")
+        {
+            q.front();
+        }
+        else if (a == "back")
+        {
+            cout << q.back() << endl;
+        }
+    }
+
+    return 0;
+}*/
+
+// 백준 9012
+/*#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+string vps(string s)
+{
+
+    stack<char> st;
+    for (int i = 0; i < s.size(); i++)
+    {
+
+        if (s[i] == '(')
+        {
+            st.push(s[i]);
+        }
+        else
+        {
+            if (!st.empty())
+                st.pop();
+            else
+                return "NO";
+        }
+    }
+
+    if (st.empty())
+        return "YES";
+    else
+        return "NO";
+}
+
+int main()
+{
+
+    string s;
+
+    int num;
+    cin >> num;
+
+    for (int i = 0; i < num; i++)
+    {
+
+        cin >> s;
+        cout << vps(s) << endl;
+    }
+    return 0;
+}*/
+
+// 백준 1920
+/*#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    cin.ignore();
+    string s;
+
+    getline(cin, s);
+    s.erase(remove(s.begin(), s.end(), ' '), s.end());
+
+    int m;
+    cin >> m;
+
+    cin.ignore();
+    string s1;
+
+    getline(cin, s1);
+    s1.erase(remove(s1.begin(), s1.end(), ' '), s1.end());
+
+    for (int i = 0; i < s1.size(); i++)
+    {
+        int cnt = 0;
+
+        for (int j = 0; j < s.size(); j++)
+        {
+            if (s1[i] == s[j])
+            {
+                cnt++;
+            }
+        }
+        if (cnt >= 1)
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+
+    return 0;
+}*/
+
+// 백준 1920
+/*#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[100000];
+int N;
+int M;
+int n;
+
+int solve(int num);
+
+int main(){
+
+ios::sync_with_stdio(false);
+cin.tie(NULL);
+
+cin>>N;
+
+for(int i=0;i<N;i++){
+    cin>>arr[i];
+}
+
+sort(arr,arr+N);
+
+int M;
+cin>>M;
+
+for(int j=0;j<M;j++){
+int M;
+cin>>M;
+cout<<solve(M)<<'\n;
+}
+    return 0;
+}
+
+int solve(int num){
+
+int low=0;
+int high=N-1;
+
+while(low<=high)
+{
+    int mid=(low+high)/2;
+    if(arr[mid]==num)
+    return true;
+    else if(num<arr[mid]) high=mid-1;
+    else if(num>arr[mid]) low=mid+1;
+
+}
+return 0;
+} */
+
+// 백준 10773
+/*#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int num;
+int money;
+
+int main()
+{
+    int total=0;
+    int size=0;
+
+    stack<int> st;
+
+    cin >> num;
+
+    for (int i = 0; i < num; i++)
+    {
+        cin >> money;
+        if (money == 0)
+            st.pop();
+        else
+            st.push(money);
+    }
+
+    size=st.size();
+
+    if (st.empty() == true)
+    {
+        cout<<"0\n";
+
+    }
+    else
+    {
+        for (int j = 0; j <size; j++)
+        {
+
+            total = total + st.top();
+            st.pop();
+            
+        }
+        cout << total<<endl;
+    }
+    
+    return 0;
+}*/
+
+//백준 216
+/*#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(){
+
+queue<int>q;
+
+int num;
+cin>>num;
+
+for(int i=1;i<=num;i++){
+    q.push(i);
+}
+
+
+while(q.size()!=1){
+
+q.pop();
+q.push(q.front());
+q.pop();
+}
+cout<<q.front()<<endl;
+
+    return 0;
+} */
+
+
+
+
+
+
+
+
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+int n;
+cin>>n;
+
+int num;
+
+vector<int>st(n);
+
+for(int i=1;i<=n;i++){
+
+cin>>num;
+
+for(int j=1;j<=num+1;j++){
+
+if(st.back()==num){
+cout<<"-\n";
+st.pop_back();
+}
+else{
+st.push_back(j);
+cout<<"+\n";
+}
+
+}
+
+}
+    return 0;
+}
